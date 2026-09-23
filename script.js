fbd919f55fd74435fbb2fc4234868444b00d02de
Ulya <yourimaginaryfriend10@gmail.com>
2026-09-23 21:59:40 +0300

    fix: fix theme switcher

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const switcher = document.querySelector('.theme-switcher');
 const root = document.documentElement;
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+
+if (root.getAttribute('data-theme') === 'dark') {
+  moon.classList.add('active');
+  sun.classList.remove('active');
+} else {
+  sun.classList.add('active');
+  moon.classList.remove('active');
+}
 
 switcher.addEventListener('click', () => {
     const isDark = root.getAttribute('data-theme') === 'dark';
@@ -7,8 +17,12 @@ switcher.addEventListener('click', () => {
     if (isDark) {
         root.removeAttribute('data-theme');
         localStorage.setItem('theme', 'light');
+        moon.classList.remove('active');
+        sun.classList.add('active');
     } else {
         root.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+        moon.classList.add('active');
+        sun.classList.remove('active');
     }
 });
